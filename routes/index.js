5a87bf844758dd0296d8b9d7d397f7a62747eccf
Akash Shekara <akashshekara1212@gmail.com>
2015-11-30 15:06:35 -0600

    added homepage

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,53 @@
 var express = require('express');
 var router = express.Router();
 var brain = require('brain');
-var Position = require('../models/position');
+var User = require('../models/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	var net = new brain.NeuralNetwork();
-	net.train([{input: { chelsea: 0.8, bayern: 0.2 }, output: { chelsea: 1 }},
-	           {input: { chelsea: 0.9, bayern: 0.1 }, output: { bayern: 1 }},
-	           {input: { chelsea: 0.3, bayern: 0.7 }, output: { bayern: 1 }}]);
-	var output = net.run({ chelsea: 0.5, bayern: 0.5 });
-	console.log(output);
-	
 	res.render('index', { title: 'Express' });
+});
+
+router.post('/', function(req, res, next) {
+	User.count({facebookId: req.body.id}, function (err, count){ 
+    if(count>0){
+      //document exists });
+    }
+    else{
+	    var newUser = new User({name: 'Akash',facebookId: req.body.id, project: []});
+			newUser.save(function (err, fluffy) {
+			  if (err) return console.error(err);
+			});
+		}
+		res.redirect('/homepage');
+	}); 
 });
 
 router.get('/template', function(req, res, next) {
 	res.render('template', { funny: 'hahahahah' });
+});
+
+router.get('/homepage', function(req, res, next) {
+  res.render('homepage',{});
+});
+
+router.post('/list', function(req, res, next) {
+	User.find({facebookId: req.body.id},function (err, count){
+		var stuff = count[0].project;
+		res.json({stuff: stuff});
+	});
+});
+
+router.post('/newproject', function(req,res,next){
+	User.findOneAndUpdate({facebookId: req.body.id},{$push: {project: {name: req.body.name, code: 0}}},{safe: true, upsert: true},function(err, model) {
+		if(err) console.log(err);
+    res.redirect('/homepage');
+  });
+});
+
+
+router.post('/homepage', function(req, res, next) {
+  res.render('homepage',{funny: 'hahahahah'});
 });
 
 module.exports = router;
